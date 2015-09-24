ea7ddd63efc5e6ecb9045b97819ddd0cb4ef21c7
liuxh <liuxinghao@wangfujing.com>
2015-09-24 16:23:22 +0800

    add url requester use https protocol

--- weixin/weixin-web/src/main/java/com/nogemasa/weixin/web/controller/BarCodeServiceController.java
+++ weixin/weixin-web/src/main/java/com/nogemasa/weixin/web/controller/BarCodeServiceController.java
@@ -6,8 +6,6 @@ import org.jbarcode.encode.InvalidAtributeException;
 import org.jbarcode.paint.BaseLineTextPainter;
 import org.jbarcode.paint.WidthCodedPainter;
 import org.jbarcode.util.ImageUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
